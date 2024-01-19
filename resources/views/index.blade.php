4870afc2a62b3c6319635f5113d01fb4903b0038
yahya <Yahyahamdi650@gmail.com>
2024-01-19 16:34:57 +0100

    modify showing the posts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,49 +1,15 @@
-@php
-    use Illuminate\Support\Facades\Crypt;
-@endphp
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <title>Document</title>
-</head>
-
-<body class="container my-5">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Image</th>
-                <th>titre</th>
-                <th>descreption</th>
-                <th>created at</th>
-                <th>updated at</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($posts as $post)
-                <tr>
-                    <td>{{ $post->image ? 'Yes' : 'No' }}</td>
-                    <td>{{ $post->titre }}</td>
-                    <td>{{ $post->description }}</td>
-                    <td>{{ $post->created_at }}</td>
-                    <td>{{ $post->updated_at }}</td>
-                    <td>
-                        <a class="btn btn-primary" href="{{ route('posts.show', Crypt::encrypt($post->id)) }}">Show
-                            Post
-                        </a>
-                    </td>
-
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('content')
+    <div class="d-flex flex-wrap gap-3 justify-content-center">
+        @foreach ($posts as $post)
+            <div class="card" style="width: 18rem;">
+                <img src="{{ $post->image }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->titre }}</h5>
+                    <p class="card-text">{{ $post->description }}</p>
+                    <p class="card-text">Created at {{ $post->created_at }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,40 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-
-    <title>Document</title>
-</head>
-
-<body class="container my-5">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Image</th>
-                <th>titre</th>
-                <th>descreption</th>
-                <th>created at</th>
-                <th>updated at</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                @if ($post->image)
-                    <td>{{ $post->image ? 'Yes' : 'No' }}</td>
-                @endif
-                <td>{{ $post->titre }}</td>
-                <td>{{ $post->description }}</td>
-                <td>{{ $post->created_at }}</td>
-                <td>{{ $post->updated_at }}</td>
-            </tr>
-        </tbody>
-    </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
+@extends('layouts.app')
+@section('content')
+    <div class="card" style="width: 18rem;">
+        <img src="{{ $post->image }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ $post->titre }}</h5>
+            <p class="card-text">{{ $post->description }}</p>
+            <p class="card-text">Created at {{ $post->created_at }}</p>
+        </div>
+    </div>
+@endsection
